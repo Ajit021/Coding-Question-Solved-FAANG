35fdbdcb4854d870c08eb1cc2fb0281d66661068
Ajit Kumar Singh <31788344+Ajit021@users.noreply.github.com>
2022-06-02 22:54:21 +0530

    Time: 40 ms (46.40%), Space: 10.8 MB (44.00%) - LeetHub

--- 77-combinations/77-combinations.cpp
+++ 77-combinations/77-combinations.cpp
@@ -1,11 +1,8 @@
 class Solution {
 public:
-    void pp(int index,vector<vector<int>> & v, int n,int k,vector<int>a,vector<int>&aux,int size){
+    void pp(int index,vector<vector<int>> & v, int n,int k,vector<int>&aux,int size){
         
-         // if(index==n){
-         //    // cout<<"kkamksm";
-         //     return;
-         // }
+      
         
         if(size==k){
             
@@ -17,11 +14,11 @@ public:
             v.push_back(q);
             return;
         }
-        //cout<<size<<"\n";
+       
         for(int j=index;j<n;j++){
-            aux[size]=a[j];
+            aux[size]=j+1;
             
-            pp(j+1,v,n,k,a,aux,size+1);
+            pp(j+1,v,n,k,aux,size+1);
         }
         
         
@@ -34,12 +31,12 @@ public:
         vector<vector<int>> v;
         int index=0;
         vector<int>a;
-        for(int i=0;i<n;i++){
-            a.push_back(i+1);
-        }
+        // for(int i=0;i<n;i++){
+        //     a.push_back(i+1);
+        // }
         
        vector<int>aux(k+2);
-        pp(index,v,n,k,a,aux,0);
+        pp(index,v,n,k,aux,0);
         return v;
     }
 };
